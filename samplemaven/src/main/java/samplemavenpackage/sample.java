025c01aa430dfe75f43c3da921e89405dcff2628
rehna <rehna@REHNA_NAZEER>
2024-11-14 22:22:43 +0530

    initial commit

---
+++ samplemaven/src/main/java/samplemavenpackage/sample.java
@@ -0,0 +1,11 @@
+package samplemavenpackage;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s="hello world";
+System.out.println(s);
+	}
+
+}
